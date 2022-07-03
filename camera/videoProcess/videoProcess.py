54ce05825f1d572286a393d9b76ac4bb52af7a83
echo-xiao9 <819601193@qq.com>
2022-07-03 17:42:38 +0800

    add two image to one win

--- camera/videoProcess/videoProcess.py
+++ camera/videoProcess/videoProcess.py
@@ -3,6 +3,9 @@ import numpy as np
 import cv2 as cv
 # from datetime import datetime
 import time
+import matplotlib.pyplot as plt
+import cvui
+
 
 areaThreshold = 0.003
 cap = cv.VideoCapture('../clips/IMG_1086.MOV')
@@ -22,7 +25,7 @@ def startCapture(thresh):
     global findFirstLed
     global lastBlinkTime
     global firstLEDTime
-    
+
     whiteAreaSize = sum(sum(thresh))
     whiteProportion = whiteAreaSize / (thresh.shape[0]*thresh.shape[1])
     if(whiteProportion >= areaThreshold):
@@ -40,7 +43,8 @@ def canCapture():
     if findFirstLed is False:
         return False
     else:
-        timeAfterFirstBlink = int(round(time.time() * 1000)) - firstLEDTime - start_time
+        timeAfterFirstBlink = int(
+            round(time.time() * 1000)) - firstLEDTime - start_time
         if (timeAfterFirstBlink - lastBlinkTime) > blinkingInterval/2:
             # print("Blinking"+str(timeAfterFirstBlink))
 
@@ -51,14 +55,15 @@ def canCapture():
                 return True
     return False
 
-# find the center of the white area
-def findCenter(img):
+# find the center of the led area and draw the circle
+
+
+def findCenter(img, resultImg):
     # convert image to grayscale image
     gray_image = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 
     # convert the grayscale image to binary image
-    ret,thresh = cv.threshold(gray_image,230,255,0)
-
+    ret, thresh = cv.threshold(gray_image, 230, 255, 0)
     # calculate moments of binary image
     M = cv.moments(thresh)
 
@@ -67,16 +72,25 @@ def findCenter(img):
     cY = int(M["m01"] / M["m00"])
 
     # put text and highlight the center
-    cv.circle(img, (cX, cY), 20, (0, 0, 255), -1)
-    cv.putText(img, "centroid", (cX - 25, cY - 25),cv.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
-    cv.imshow("Image", img)
+    # cv.circle(img, (cX, cY), 20, (0, 0, 255), -1)
+    cv.putText(img, "centroid", (cX - 25, cY - 25),
+               cv.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
 
+    # put text and highlight the center
+    cv.circle(resultImg, (cX, cY), 10, (255, 255, 255), -1)
+    # cv.imshow("Image", resultImg)
+    return resultImg
 
-    
 def main():
+   
     global findFirstLed
+    # concatenate image Horizontally
+    resultImgRGB = np.zeros((1920, 1080, 3), np.uint8)
+
     while cap.isOpened():
         ret, frame = cap.read()
+        
+
         # if frame is read correctly ret is True
         if not ret:
             print("Can't receive frame (stream end?). Exiting ...")
@@ -84,19 +98,22 @@ def main():
 
         if findFirstLed == False:
             gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+            resultImg = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
             # filter the image with a threshold
             ret, thresh = cv.threshold(gray, 230, 255, cv.THRESH_BINARY)
-            cv.imshow('frame', gray)
+            # cv.imshow('Image', gray)
             findFirstLed = startCapture(thresh)
         else:
             if canCapture() is True:
                 # show the image
-                cv.imshow('frame', frame)
-                findCenter(frame)
+                resultImg = findCenter(frame, resultImg)
+                resultImgRGB = cv.cvtColor(resultImg,cv.COLOR_GRAY2RGB)
+
+        Hori = np.concatenate((frame, resultImgRGB), axis=1)
+
+        cv.imshow('HORIZONTAL', Hori)
         if cv.waitKey(1) == ord('q'):
             break
-
-    # every one second capture the image from the video stream
 
     cap.release()
     cv.destroyAllWindows()
