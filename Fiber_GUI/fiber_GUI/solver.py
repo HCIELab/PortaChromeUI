7c47e2998761236c5636933483cee00bc67668e5
kangyixiao <kangyixiao@bytedance.com>
2023-09-08 22:42:00 -0700

    save before merge

--- Fiber_GUI/fiber_GUI/solver.py
+++ Fiber_GUI/fiber_GUI/solver.py
@@ -1,7 +1,8 @@
 # Echo server program
 import socket
 import numpy as np
-from scipy import optimize
+from scipy.optimize import minimize
+
 
 
 # FULL_DEACTIVATION_TIME = (
@@ -19,6 +20,7 @@ FULL_DEACTIVATION_TIME = [[3500,2000,3600],[2000,2000,1000],[inf,4500,60]]
 
 RGB_SCALE = 255
 CMYK_SCALE = 1
+
 def rgb_to_cmyk(r, g, b):
     if (r, g, b) == (0, 0, 0):
         # black
@@ -39,68 +41,12 @@ def rgb_to_cmyk(r, g, b):
     # rescale to the range [0,CMYK_SCALE]
     return c * CMYK_SCALE, m * CMYK_SCALE, y * CMYK_SCALE, k * CMYK_SCALE
 
-
 # convert cmyk to rgb color
 def cmyk_to_rgb(c,m,y,k):
     r = RGB_SCALE*(1-c)*(1-k)
     g = RGB_SCALE*(1-m)*(1-k)
     b = RGB_SCALE*(1-y)*(1-k)
     return int(r), int(g), int(b)
-
-# solve Ax = b
-# get the closest value that it gives while
-# keeping x non-negative
-def linear_programming_solve(A, b):
-    '''
-  The linear progression is like this: 
-    min(y0 + y1 + y2)
-    when 
-      y0 >= (Ax-b)[0]
-      y0 >= -(Ax-b)[0]
-      y1 >= (Ax-b)[1]
-      y1 >= -(Ax-b)[1]
-      y2 >= (Ax-b)[2]
-      y3 >= -(Ax-b)[2]
-  We then convert y0, y1, y2 to x3, x4, x5, respectively
-  and then solve to fit the scipy API
-  here: https://docs.scipy.org/doc/scipy/reference/generated/scipy.optimize.linprog.html
-  input 有cmy,
-  y0 = |c-c'| -> Y0>= c-c'; y0>= c'-c
-  y1 = |M-M'|
-  y2 = |Y-Y‘|
-  我们希望色差最小-> y0+y1+y2最小
-  梯度下降可能更好，用y0^2 + y1^2 +y3^2
-  
-
-  
-  '''
-    c = [0, 0, 0, 1, 1, 1]
-    A_ub = []
-    for i in range(6):
-        row = []
-        for j in range(3):
-            if i % 2 == 0:
-                row.append(A[i // 2][j])
-            else:
-                row.append(-A[i // 2][j])
-        for j in range(3, 6):
-            row.append(0)
-        row[i // 2 + 3] = -1
-        A_ub.append(row)
-    b_ub = [b[0], -b[0], b[1], -b[1], b[2], -b[2]]
-    bounds = [(0, None), (0, None), (0, None), (None, None), (None, None),
-              (None, None)]
-
-    results = optimize.linprog(c=c, A_ub=A_ub, b_ub=b_ub, bounds=bounds)
-    # return ({
-    #     "x": [results.x[0], results.x[1], results.x[2]],
-    #     "error": results.fun,
-    #     "deltaColor": [results.x[3], results.x[4], results.x[5]]
-    # })
-    deltaColor = [results.x[3], results.x[4], results.x[5]]
-    deactivation_time = [results.x[0], results.x[1], results.x[2]]
-
-    return deactivation_time, deltaColor
 
 class Deactivation:
     debug = False
@@ -114,20 +60,37 @@ class Deactivation:
             deactivation_speed.append(row)
         self.deactivation_speed = np.array(deactivation_speed)
 
+
     def compute_deactivation_time(self,
                                   target_color,
                                   original_color=[1, 1, 1]):
         color_to_deactivate = np.array(original_color) - np.array(target_color)
-        deactivation_time, deltaColor = linear_programming_solve(self.deactivation_speed,
-                                                     color_to_deactivate)
-        realColor =[]                                             
-        for i in range(3):
-            deltaColorWithReal = 0
-            for j in range(3):
-                deltaColorWithReal += (1/FULL_DEACTIVATION_TIME[i][j])* deactivation_time[j]
-            realColor.append(original_color[i] - deltaColorWithReal)
-        return deactivation_time, realColor
- 
+        
+        A = self.deactivation_speed
+        b = color_to_deactivate
+
+        def objective(x):
+            return np.linalg.norm(A.dot(x) - b)**2
+
+        # Initial guess
+        x0 = np.array([1, 1, 1])
+
+        # Perform optimization with constraints
+        bounds = [(0, None)] * 3
+        results = minimize(objective, x0, bounds=bounds)
+        
+
+        if results.success:
+            deactivation_time = results.x
+            realColor = A.dot(deactivation_time)
+            return deactivation_time, realColor
+        else:
+            print("Optimization failed")
+            exit(  "Optimization failed")
+            return None, None
+    
+
+
 HOST = ''                 # Symbolic name meaning all available interfaces
 PORT = 50007              # Arbitrary non-privileged port
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -137,6 +100,7 @@ conn, addr = s.accept()
 print('Connected by', addr)
 dataSent=""
 ledNum=0
+
 while True:
     data = conn.recv(1024).decode("utf-8")
     # if data is not None:
@@ -170,13 +134,6 @@ while True:
         fwirte.close()
         # send dataSent to socket 
         conn.send("0\n".encode("utf-8"))
-    
-    
-
-
-
-
-
 
 
 
